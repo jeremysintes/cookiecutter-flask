dac0b100842dd70b5a7feb406d39c8a2d52d6aca
SINTES Jeremy <jeremy.sintes@soprasteria.com>
2017-10-20 16:17:33 +0200

    redirecting the sphinx creation file to be included in the dev_site

--- {{cookiecutter.project_slug}}/docs/make.bat
+++ {{cookiecutter.project_slug}}/docs/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=_build
+set BUILDDIR=../{{cookiecutter.project_slug}}_devsite/_site/docs
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
