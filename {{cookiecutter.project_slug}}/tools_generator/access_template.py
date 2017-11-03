fd4f84b2f664dc61a6684cfc4f0bb9e9996da321
SINTES Jeremy <jeremy.sintes@soprasteria.com>
2017-11-03 14:23:23 +0100

    updating files concerned by confidential information

---
+++ {{cookiecutter.project_slug}}/tools_generator/access_template.py
@@ -0,0 +1,5 @@
+# TRELLO
+# Complete the key and token below with your access. 
+# You can find yours at: https://trello.com/app-key (be sure to be loggued to trello)
+trello_access = {"key": "", "token" : ""}
+
